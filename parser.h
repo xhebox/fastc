9c653c634570100329c50688b3c66019920edc16
xhe <xw897002528@gmail.com>
2017-03-05 19:46:37 +0800

    add comment in header file

--- parser.h
+++ parser.h
@@ -12,6 +12,9 @@ extern "C" {
 
 #include <stdint.h>
 
+// para ( input array, output array, inputlen)
+// we do not check if output array is big enough, but it sould be smaller than inputlen
+// and if it's a seq like ANANAN, too much fragmentation, it may be bigger than inputlen
 extern int fc_encode(uint8_t*, uint8_t*, uint64_t);
 extern int fc_decode(uint8_t*, uint8_t*, uint64_t);
 
